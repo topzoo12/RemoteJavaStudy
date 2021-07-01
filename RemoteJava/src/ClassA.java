2dc130e9697d7ad48a0fcc8d09f9638e1b2e9061
user <user@DESKTOP-7EQF3M8>
2021-07-01 16:05:08 +0900

    ClassA ClassC 만 스테이지에 commit

--- RemoteJava/src/ClassA.java
+++ RemoteJava/src/ClassA.java
@@ -3,4 +3,8 @@ public class ClassA {
 	public int methodA() {
 		return 10;
 	}
+	
+	public int methodB() {
+		return 10;
+	}
 }
---
+++ RemoteJava/src/ClassC.java
@@ -0,0 +1,7 @@
+
+public class ClassC {
+
+	public int methodA(int a, int b, int c) {
+		return a * b * c;
+	}
+}
